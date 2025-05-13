ba257b0b4afed1a39549e3f1d540932a9e4b5053
caufieldjh <j.harry.caufield@gmail.com>
2025-05-13 13:56:26 -0400

    CLI improvements

--- src/trowel/cli.py
+++ src/trowel/cli.py
@@ -11,7 +11,10 @@ __all__ = [
     "main",
 ]
 
-path_option = click.option("-p", "--path", help="Path to a file or directory.")
+path_option = click.option(
+    "-p", "--path", help="Path to a file or directory.", required=False)
+outpath_option = click.option(
+    "-o", "--outpath", help="Directory where output files should be written.", required=False, default=".")
 
 # Get token from environment
 ESSDIVE_TOKEN = os.getenv("ESSDIVE_TOKEN")
@@ -37,11 +40,22 @@ def main():
 
 @main.command()
 @path_option
-def get_essdive_metadata(path):
-    """Given a file containing one DOI per line, return metadata from ESS-DIVE."""
+@outpath_option
+def get_essdive_metadata(path, outpath):
+    """Given a file containing one DOI per line, return metadata from ESS-DIVE.
+    Parameters:
+    path: Path to a file containing DOIs, without the doi prefix, one per line.
+    outpath: Directory where output files should be written.
+    """
 
     if not path:
-        logging.error("You must provide a path to a file containing DOIs.")
+        logging.error(
+            "You must provide a path to a file containing DOIs with the --path option.")
+        sys.exit()
+
+    if not os.path.exists(outpath):
+        logging.error(
+            f"The specified output directory '{outpath}' does not exist.")
         sys.exit()
 
     with open(path, "r") as f:
@@ -49,32 +63,58 @@ def get_essdive_metadata(path):
 
     results, frequencies, filetable = get_metadata(identifiers, ESSDIVE_TOKEN)
 
-    with open("frequencies.txt", "w") as freq_file:
+    with open(os.path.join(outpath, "frequencies.txt"), "w") as freq_file:
         for freq in frequencies:
             freq_file.write(f"{freq}\t{frequencies[freq]}\n")
 
-    with open("results.txt", "w") as results_file:
+    with open(os.path.join(outpath, "results.txt"), "w") as results_file:
         results_file.write(str(results))
 
-    with open("filetable.txt", "w") as filetable_file:
+    with open(os.path.join(outpath, "filetable.txt"), "w") as filetable_file:
         filetable_file.write(str(filetable))
 
 
 @main.command()
-def get_essdive_column_names():
+@path_option
+@outpath_option
+def get_essdive_column_names(path, outpath):
     """Get all column names from all data files.
     Files are those with identifiers retrieved by the get_essdive_metadata function.
-    By default, this is filetable.txt."""
+    By default, this is filetable.txt.
+    Parameters:
+    path: Path to a directory containing results from get_essdive_metadata.
+    outpath: Directory where output files should be written. If not provided, defaults to the input path.
+    """
 
-    filetable_path = "filetable.txt"
+    if not path:
+        # Check current directory for filetable.txt if path not provided
+        current_dir = os.getcwd()
+        if os.path.exists(os.path.join(current_dir, "filetable.txt")):
+            path = current_dir
+            logging.info(f"Using current directory '{current_dir}' as input path.")
+        else:
+            logging.error(
+                "No path provided and filetable.txt not found in current directory. Please use --path option.")
+            sys.exit()
+
+    if not outpath:
+        outpath = path
+        logging.info(
+            f"No output path provided. Using input path '{path}' as output path.")
+    if not os.path.exists(outpath):
+        logging.error(
+            f"The specified output directory '{outpath}' does not exist.")
+        sys.exit()
+
+    filetable_path = os.path.join(outpath, "filetable.txt")
 
     if not os.path.exists(filetable_path):
-        logging.error("You must run get_essdive_metadata first to get the filetable.")
+        logging.error("Couldn't find a filetable at the provided path.")
         sys.exit()
 
     column_names = get_column_names(filetable_path)
 
-    with open("column_names.txt", "w") as filetable_file:
+    with open(os.path.join(outpath, "column_names.txt"), "w") as filetable_file:
         for colname in column_names:
             filetable_file.write(f"{colname}\t{column_names[colname]}\n")
 
